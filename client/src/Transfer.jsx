fcace8a4c474fc33a7454dca6c55c4630170198e
0xpantera <0xpantera@proton.me>
2022-12-15 19:14:08 -0600

    Send signed message and derived eth address to server

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 import server from "./server";
 
-function Transfer({ address, setBalance }) {
+import { toHex, utf8ToBytes } from "ethereum-cryptography/utils";
+import * as secp from "ethereum-cryptography/secp256k1";
+import { keccak256 } from "ethereum-cryptography/keccak";
+
+function Transfer({ setBalance, privateKey }) {
   const [sendAmount, setSendAmount] = useState("");
   const [recipient, setRecipient] = useState("");
 
@@ -10,18 +14,39 @@ function Transfer({ address, setBalance }) {
   async function transfer(evt) {
     evt.preventDefault();
 
-    try {
-      const {
-        data: { balance },
-      } = await server.post(`send`, {
-        sender: address,
-        amount: parseInt(sendAmount),
-        recipient,
-      });
-      setBalance(balance);
-    } catch (ex) {
-      alert(ex.response.data.message);
+    // Ensure Private Key is valid
+    if (privateKey.length == 64) {
+
+      // Derive Address
+      const publicKey = secp.getPublicKey(privateKey);
+      const address = toHex(keccak256(publicKey.slice(1)).slice(-20));
+
+      // Generate Transaction Message
+      var message = { 'to': recipient, 'amount': parseInt(sendAmount) };
+      var messageHash = keccak256(utf8ToBytes(JSON.stringify(message)));
+
+      // Sign Message and get Recovery Bit
+      var [signature, recoveryBit] = secp.signSync(messageHash, privateKey, { recovered: true });
+
+      console.log(signature);
+
+      try {
+        const {
+          data: { balance },
+        } = await server.post(`send`, {
+          sender: address,
+          signature: [signature, recoveryBit],
+          message: message,
+          amount: parseInt(sendAmount),
+          recipient,
+        });
+        setBalance(balance);
+      } catch (ex) {
+        alert(ex.response.data.message);
+      }
     }
+
+
   }
 
   return (
@@ -50,5 +75,4 @@ function Transfer({ address, setBalance }) {
     </form>
   );
 }
-
 export default Transfer;
